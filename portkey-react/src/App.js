9a0b01c9e2631a61908f9c0097612b3989a1da25
Jared Warren <jared.warren@gmail.com>
2021-09-07 08:33:36 -0500

    Add Login dialog setup.

--- portkey-react/src/App.js
+++ portkey-react/src/App.js
@@ -41,6 +41,9 @@ function App() {
   const [signupUsername, setSignupUsername] = React.useState(null);
   const [signupEmail, setSignupEmail] = React.useState(null);
   const [signupPassword, setSignupPassword] = React.useState(null);
+  const [messageDialogText, setMessageDialogText] = React.useState(null);
+  const [loginUsername, setLoginUsername] = React.useState(null);
+  const [loginPassword, setLoginPassword] = React.useState(null);
 
 
   const handleClickMainMenu = (event) => {
@@ -96,17 +99,37 @@ function App() {
       const data = await cognitoIdpClient.send(command);
       console.log("Signup attempt complete")
       console.log(data)
+      let httpStatusCode = data.$metadata.httpStatusCode
+      let verified = data.UserConfirmed
+      let message = ""
+      if (verified) {
+        message = "User signup complete. Please login."
+      } else {
+        message = "User signup complete, but your account is not verified. "
+        let deliveryMethod = data.CodeDeliveryDetails.DeliveryMedium
+        if (deliveryMethod == "EMAIL") {
+          message += "Please check your email for verification instructions."
+        } else if (deliveryMethod == "SMS") {
+          message += "Please check your text messages for verification instructions."
+        }
+      }
+      setSignupDialogAnchorEl(null)
+      setMessageDialogText(message)
     } catch (error) {
       console.log("Error while executing signup.")
       console.log(error)
+      if (error.name == "UsernameExistsException") {
+        setSignupDialogAnchorEl(null)
+        setMessageDialogText(error.message)
+      }
     } finally {
       // finally.
     }
     setSignupDialogAnchorEl(null)
   }
 
-  const handleSignupDialogSignup = (event) => {
-    executeSignup(event)
+  const handleMessageDialogClose = (event) => {
+    setMessageDialogText(null)
   }
 
   const handleSignupDialogCancel = (event) => {
@@ -117,9 +140,29 @@ function App() {
     setSignupDialogAnchorEl(null)
   }
 
+  const handleSignupDialogSignup = (event) => {
+    executeSignup(event)
+  }
+
   const handleSignupDialogClose = (event) => {
     setSignupDialogAnchorEl(null)
   }
+
+  const handleLoginDialogCancel = (event) => {
+    setMainMenuAnchorEl(null)
+    setLoginDialogAnchorEl(null)
+  }
+
+  const handleLoginDialogLogin = (event) => {
+    setMainMenuAnchorEl(null)
+    setLoginDialogAnchorEl(null)
+  }
+
+  const handleLoginDialogClose = (event) => {
+    setMainMenuAnchorEl(null)
+    setLoginDialogAnchorEl(null)
+  }
+
 
   return (
     <Grid container direction="column" justifyContent="center" alignItems="center">
@@ -175,16 +218,14 @@ function App() {
         <DialogTitle id="singupDialogTitle">Signup for portkey.to</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Please provide your email address, password, and a username that will be visible to those who follow your portkey marks.
+            Please provide your email address, password, and a username that will be visible to users of the site.
           </DialogContentText>
           <TextField
-            autoFocus
-            value={signupEmail}
-            onChange={(e) => setSignupEmail(e.target.value)}
+            value={signupUsername}
+            onChange={(e) => setSignupUsername(e.target.value)}
             margin="dense"
-            id="signupEmailField"
-            label="Email Address"
-            type="email"
+            id="signupUsernameField"
+            label="Username"
             fullWidth />
           <TextField
             value={signupPassword}
@@ -195,16 +236,53 @@ function App() {
             type="password"
             fullWidth />
           <TextField
-            value={signupUsername}
-            onChange={(e) => setSignupUsername(e.target.value)}
+            autoFocus
+            value={signupEmail}
+            onChange={(e) => setSignupEmail(e.target.value)}
             margin="dense"
-            id="signupUsernameField"
-            label="Username"
+            id="signupEmailField"
+            label="Email Address"
+            type="email"
             fullWidth />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleSignupDialogSignup} color="primary">Signup</Button>
           <Button onClick={handleSignupDialogCancel} color="primary">Cancel</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog onClose={handleMessageDialogClose} open={Boolean(messageDialogText)}>
+        <DialogTitle id="messageDialogTitle">Signup for portkey.to</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {messageDialogText}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleMessageDialogClose} color="primary">Ok</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog onClose={handleLoginDialogClose} open={Boolean(loginDialogAnchorEl)}>
+        <DialogTitle id="loginDialogTitle">Login</DialogTitle>
+        <DialogContent>
+          <TextField
+            value={loginUsername}
+            onChange={(e) => setLoginUsername(e.target.value)}
+            margin="dense"
+            id="loginUsernameField"
+            label="Username"
+            fullWidth />
+          <TextField
+            value={loginPassword}
+            onChange={(e) => setLoginPassword(e.target.value)}
+            margin="dense"
+            id="loginPasswordField"
+            label="Password"
+            type="password"
+            fullWidth />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleLoginDialogLogin} color="primary">Login</Button>
+          <Button onClick={handleLoginDialogCancel} color="primary">Cancel</Button>
         </DialogActions>
       </Dialog>
     </Grid>
